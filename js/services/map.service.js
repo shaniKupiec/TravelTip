678e03a557a2359ca30a74630662519f89c4243f
Roy Ben Aviv <roybenavivv@gmail.com>
2022-02-23 15:28:13 +0200

    -- added first css files and layout -- added API Key for google map

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -12,48 +12,48 @@ var gMap;
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap');
     return _connectGoogleApi()
-        .then(() => {
-            console.log('google available');
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
+    .then(() => {
+        console.log('google available');
+        gMap = new google.maps.Map(
+            document.querySelector('#map'), {
                 center: { lat, lng },
                 zoom: 15
             })
             console.log('Map!', gMap);
         })
-}
-
-
-// adds marker according to chosen loc
-function addMarker(loc) {
-    var marker = new google.maps.Marker({
-        position: loc,
-        map: gMap,
-        title: 'Hello World!'
-    });
-    return marker;
-}
-
-// shows new loc on map
-function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng(lat, lng);
-    gMap.panTo(laLatLng);
-}
-
-// inner func that upload map for first 
-// TODO: add API
-// WHY: window.google??
-function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
-    var elGoogleApi = document.createElement('script');
-    // console.log(elGoogleApi);
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
-    elGoogleApi.async = true;
-    document.body.append(elGoogleApi);
-
-    return new Promise((resolve, reject) => {
-        elGoogleApi.onload = resolve;
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
-}
+    }
+    
+    
+    // adds marker according to chosen loc
+    function addMarker(loc) {
+        var marker = new google.maps.Marker({
+            position: loc,
+            map: gMap,
+            title: 'Hello World!'
+        });
+        return marker;
+    }
+    
+    // shows new loc on map
+    function panTo(lat, lng) {
+        var laLatLng = new google.maps.LatLng(lat, lng);
+        gMap.panTo(laLatLng);
+    }
+    
+    // inner func that upload map for first 
+    // TODO: add API
+    // WHY: window.google??
+    function _connectGoogleApi() {
+        const API_KEY = 'AIzaSyAJfCM2o8DvIAvQdLQcfjKiQpUYOdm0vO0'
+        if (window.google) return Promise.resolve()
+        var elGoogleApi = document.createElement('script');
+        // console.log(elGoogleApi);
+        elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+        elGoogleApi.async = true;
+        document.body.append(elGoogleApi);
+        
+        return new Promise((resolve, reject) => {
+            elGoogleApi.onload = resolve;
+            elGoogleApi.onerror = () => reject('Google script failed to load')
+        })
+    }
